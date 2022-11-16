5f8fbb9158b8e25ffc0bcd33745b9d840de9a09d
leo-marsano <lm010009@infolab01>
2022-11-16 18:17:02 +0100

    added new file

---
+++ esercitazione_6/copernicus.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from scipy import constants, fft
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+
+#tab
+tab = pd.read_csv('/home/lm010009/Scrivania/get-mcf-data/get_data.py/copernicus_PG_selected.csv')
+print(tab)
